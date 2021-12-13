feb686635ea250135dee6d0aa7c5994a1cf25e9e
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2021-12-13 17:52:23 +0400

    deploying celery

--- news/settings.py
+++ news/settings.py
@@ -82,9 +82,13 @@ WSGI_APPLICATION = "news.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "news_db"),
+        "USER": os.getenv("POSTGRES_USER", "test_user"),
+        "PORT": os.getenv("POSTGRES_PORT", 5432),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "12345"),
+        "HOST": os.getenv("POSTGRES_HOST", "127.0.0.1"),
     }
 }
 
